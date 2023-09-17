6601dc39092c707a03945fa38d30e27f331ceaa0
ssshooter <ssshooterx@gmail.com>
2023-09-17 22:47:30 +0800

    chore: update version

--- src/index.ts
+++ src/index.ts
@@ -130,7 +130,7 @@ MindElixir.DARK_THEME = DARK_THEME
  * @memberof MindElixir
  * @static
  */
-MindElixir.version = '3.1.1'
+MindElixir.version = '3.1.2'
 /**
  * @function
  * @memberof MindElixir
