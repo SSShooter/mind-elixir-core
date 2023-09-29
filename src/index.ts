1e55e9ad14d745426df71eeac865f101df1773ea
ssshooter <ssshooterx@gmail.com>
2023-09-29 14:27:57 +0800

    build: update version number

--- src/index.ts
+++ src/index.ts
@@ -126,7 +126,7 @@ MindElixir.DARK_THEME = DARK_THEME
  * @memberof MindElixir
  * @static
  */
-MindElixir.version = '3.2.0'
+MindElixir.version = '3.2.1'
 /**
  * @function
  * @memberof MindElixir
