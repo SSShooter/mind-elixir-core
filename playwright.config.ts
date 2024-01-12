03b1520bdb83d3bc8fbcaa7da10044b0c66bb19f
ssshooter <ssshooterx@gmail.com>
2024-01-12 22:24:50 +0800

    ci: update playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -70,9 +70,11 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://127.0.0.1:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
+  webServer: {
+    command: 'npm run dev',
+    url: 'http://127.0.0.1:23333',
+    reuseExistingServer: !process.env.CI,
+    stdout: 'ignore',
+    stderr: 'pipe',
+  },
 })
