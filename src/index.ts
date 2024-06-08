0bad31c6fd985cb35dc2990202899271fbaf4d3d
ssshooter <ssshooterx@gmail.com>
2024-06-08 11:51:39 +0800

    build: update version

--- src/index.ts
+++ src/index.ts
@@ -129,7 +129,7 @@ MindElixir.DARK_THEME = DARK_THEME
  * @memberof MindElixir
  * @static
  */
-MindElixir.version = '4.0.1'
+MindElixir.version = '4.0.3'
 /**
  * @function
  * @memberof MindElixir
