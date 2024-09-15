67909b06e4dcd059fa89c6737815b18712600178
ssshooter <ssshooterx@gmail.com>
2024-09-15 08:59:59 +0800

    build: v4.1.2

--- src/index.ts
+++ src/index.ts
@@ -129,7 +129,7 @@ MindElixir.DARK_THEME = DARK_THEME
  * @memberof MindElixir
  * @static
  */
-MindElixir.version = '4.1.1'
+MindElixir.version = '4.1.2'
 /**
  * @function
  * @memberof MindElixir
