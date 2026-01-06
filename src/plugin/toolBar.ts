0ba6c2a12658acc9f20e08471dd448f835da3045
DJJo <ssshooterx@gmail.com>
2026-01-06 02:58:52 +0000

    feat: enhance fullscreen handling

--- src/plugin/toolBar.ts
+++ src/plugin/toolBar.ts
@@ -1,4 +1,5 @@
 import type { MindElixirInstance } from '../types/index'
+import { getTranslate } from '../utils/index'
 import side from '../icons/side.svg?raw'
 import left from '../icons/left.svg?raw'
 import right from '../icons/right.svg?raw'
@@ -31,21 +32,68 @@ function createToolBarRBContainer(mind: MindElixirInstance) {
   const gc = createButton('toCenter', 'living')
   const zo = createButton('zoomout', 'zoomout')
   const zi = createButton('zoomin', 'zoomin')
-  const percentage = document.createElement('span')
-  percentage.innerText = '100%'
   toolBarRBContainer.appendChild(fc)
   toolBarRBContainer.appendChild(gc)
   toolBarRBContainer.appendChild(zo)
   toolBarRBContainer.appendChild(zi)
-  // toolBarRBContainer.appendChild(percentage)
   toolBarRBContainer.className = 'mind-elixir-toolbar rb'
-  fc.onclick = () => {
-    if (document.fullscreenElement === mind.el) {
-      document.exitFullscreen()
-    } else {
-      mind.el.requestFullscreen()
+
+  let fullscreenChangeData: {
+    containerRect: DOMRect
+    currentTransform: { x: number; y: number }
+    mapCenterX: number
+    mapCenterY: number
+  } | null = null
+
+  const recordCurrentState = () => {
+    const containerRect = mind.container.getBoundingClientRect()
+    const currentTransform = getTranslate(mind.map.style.transform)
+
+    const containerCenterX = containerRect.width / 2
+    const containerCenterY = containerRect.height / 2
+
+    const mapCenterX = (containerCenterX - currentTransform.x) / mind.scaleVal
+    const mapCenterY = (containerCenterY - currentTransform.y) / mind.scaleVal
+
+    fullscreenChangeData = {
+      containerRect,
+      currentTransform,
+      mapCenterX,
+      mapCenterY,
     }
   }
+
+  const handleFullscreenChange = () => {
+    if (fullscreenChangeData) {
+      const newContainerRect = mind.container.getBoundingClientRect()
+      const newContainerCenterX = newContainerRect.width / 2
+      const newContainerCenterY = newContainerRect.height / 2
+      const newTransformX = newContainerCenterX - fullscreenChangeData.mapCenterX * mind.scaleVal
+      const newTransformY = newContainerCenterY - fullscreenChangeData.mapCenterY * mind.scaleVal
+
+      const dx = newTransformX - fullscreenChangeData.currentTransform.x
+      const dy = newTransformY - fullscreenChangeData.currentTransform.y
+
+      mind.move(dx, dy)
+    }
+  }
+
+  mind.el.addEventListener('fullscreenchange', handleFullscreenChange)
+  fc.onclick = () => {
+    recordCurrentState()
+    if (document.fullscreenElement !== mind.el) {
+      mind.el.requestFullscreen()
+    } else {
+      document.exitFullscreen()
+    }
+  }
+
+  // There is an unresolvable issue here: when users exit fullscreen using the ESC key,
+  // the browser doesn't provide an opportunity to call recordCurrentState,
+  // so exiting with ESC cannot return to the center point before entering fullscreen.
+  // Although this could be implemented by periodic calculations or listening to move/zoom events, it's too complex.
+  // After weighing the options, we've decided not to implement it for now.
+
   gc.onclick = () => {
     mind.toCenter()
   }
