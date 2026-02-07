56cb873d80caba5cf8169d548f4ae54220279b35
ssshooter <ssshooterx@gmail.com>
2026-02-07 12:32:42 +0800

    feat: build & export plaintext converter

--- build.js
+++ build.js
@@ -7,19 +7,28 @@ const buildList = [
   {
     name: 'MindElixir',
     enrty: __dirname + './src/index.ts',
+    formats: ['iife', 'es'],
   },
   {
     name: 'MindElixirLite',
     enrty: __dirname + './src/index.ts',
     mode: 'lite',
+    formats: ['iife', 'es'],
   },
   {
     name: 'example',
     enrty: __dirname + './src/exampleData/1.ts',
+    formats: ['es'],
   },
   {
     name: 'LayoutSsr',
     enrty: __dirname + './src/utils/layout-ssr.ts',
+    formats: ['es'],
+  },
+  {
+    name: 'PlaintextConverter',
+    enrty: __dirname + './src/utils/plaintextConverter.ts',
+    formats: ['es'],
   },
 ]
 for (let i = 0; i < buildList.length; i++) {
@@ -32,7 +41,7 @@ for (let i = 0; i < buildList.length; i++) {
         entry: info.enrty,
         fileName: info.name,
         name: info.name,
-        formats: ['iife', 'es'],
+        formats: info.formats,
       },
       rollupOptions: {
         plugins: [
