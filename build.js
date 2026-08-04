ab9bf8a828f07f3ec8af623ad1c15041ddcdbb61
zhoudejie <zhoudejie@local.com>
2026-08-04 20:21:02 +0800

    chore(build): update build configurations and dependencies

--- build.js
+++ build.js
@@ -7,33 +7,27 @@ const buildList = [
   {
     name: 'MindElixir',
     enrty: __dirname + './src/index.ts',
-    formats: ['iife', 'es'],
   },
   {
     name: 'MindElixirLite',
     enrty: __dirname + './src/index.ts',
     mode: 'lite',
-    formats: ['iife', 'es'],
   },
   {
     name: 'example',
     enrty: __dirname + './src/exampleData/1.ts',
-    formats: ['es'],
   },
   {
     name: 'LayoutSsr',
     enrty: __dirname + './src/utils/layout-ssr.ts',
-    formats: ['es'],
   },
   {
     name: 'PlaintextConverter',
     enrty: __dirname + './src/utils/plaintextConverter.ts',
-    formats: ['es'],
   },
   {
     name: 'i18n',
     enrty: __dirname + './src/i18n.ts',
-    formats: ['es'],
   },
 ]
 for (let i = 0; i < buildList.length; i++) {
@@ -46,7 +40,7 @@ for (let i = 0; i < buildList.length; i++) {
         entry: info.enrty,
         fileName: info.name,
         name: info.name,
-        formats: info.formats,
+        formats: ['es'],
       },
       rollupOptions: {
         plugins: [
